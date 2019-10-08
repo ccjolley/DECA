ca61be46fc2e4e334a7f8c0c050c3d8e38f7b113
Craig Jolley <jolleycraig@gmail.com>
2019-10-08 17:00:07 -0400

    Add fotn.R

---
+++ fotn.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+library(readxl)
+
+fotn <- read_excel('data/FINAL_DATA_FOTN2018_TOTAL_and_CATEGORY_SCORES.xlsx',sheet=1) %>%
+  rename(country=1,access_obstacles=3,content_limits=4,user_violations=5,fotn_total=6) %>%
+  select(1,3,4,5,6)
