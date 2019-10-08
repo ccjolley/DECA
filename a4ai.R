cbaf5324032b0a930a2b771b348ad3619ed43a6f
Craig Jolley <jolleycraig@gmail.com>
2019-10-08 16:15:16 -0400

    Add a4ai.R

---
+++ a4ai.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(readxl)
+
+a4ai_pricing <- read_csv('data/A4AI-Mobile-Broadband-Pricing-2019Q2.csv') %>%
+  rename(country=Country_Name) %>%
+  select(country,Cost_1_GB_Share_GNICM)
+
+afford <- read_excel('data/2018-Affordability-Index_Indicators.xlsx',sheet=1) %>%
+  rename(country=1,access=2,infrastructure=3,overall=4) %>%
+  select(1:4)
+
+primary <- read_excel('data/2018-Affordability-Index_Indicators.xlsx',sheet=3,skip=1) %>%
+  select(Country,
+         A1, # To what extent are ICT licensing frameworks flexible, simple, and technology and service neutral?
+         A2, # To what extent does the government ICT regulator perform its functions according to published and transparent rules, with the ICT regulatory decisions influenced by public consultations?
+         A4, # To what extent is ICT regulatory decision-making informed and influenced by adequate evidence?
+         A6, # To what extent are national-level policies or rules in place to facilitate efficient access to public rights of way and tower zoning permission?
+         A10) %>% # Transparent, competitive and fair process for increasing spectrum availability 
+  rename(country=Country,neutral_licensing=A1,regulator_rules=A2,regulator_evidence=A4,zoning_policies=A6,spectrum_avail=A10)
+
+secondary <- read_excel('data/2018-Affordability-Index_Indicators.xlsx',sheet=4) %>%
+  rename(broadband_plan=4,intl_bandwidth=6,secure_servers=7,investment=9) %>%
+  select(1,4,6,7,9)
+
+a4ai <- left_join(a4ai_pricing,afford,by='country') %>%
+  left_join(primary,by='country') %>%
+  left_join(secondary,by='country')
+
+rm(a4ai_pricing,afford,primary,secondary)
