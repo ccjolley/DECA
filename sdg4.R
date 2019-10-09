fc10feee53e714762781d3fd2df286cc0403c5d9
Craig Jolley <jolleycraig@gmail.com>
2019-10-09 13:46:32 -0400

    Add gender gap calculations to sdg4.R

--- sdg4.R
+++ sdg4.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(reshape2)
+# source('utils.R')
 
 rename_df <- tibble(
   Indicator = c("Proportion of youth and adults who have copied or moved a file or folder (%)",                                                       
@@ -45,6 +46,18 @@ sdg4 <- read_csv('data/SDG4_DS_04102019154356211.csv') %>%
   left_join(rename_df,by='Indicator') %>%
   select(country,short,Value) %>%
   dcast(country ~ short) %>%
-  fix_adm0
+  mutate(copy_paste_gender_gap=(copy_paste_m-copy_paste_f)/copy_paste_m,
+         create_pres_gender_gap=(create_pres_m-create_pres_f)/create_pres_m,
+         install_soft_gender_gap=(install_soft_m-install_soft_f)/install_soft_m,
+         move_device_gender_gap=(move_device_m-move_device_f)/move_device_m,
+         move_file_gender_gap=(move_file_m-move_file_f)/move_file_m,
+         new_device_gender_gap=(new_device_m-new_device_f)/new_device_m,
+         sent_email_gender_gap=(sent_email_m-sent_email_f)/sent_email_m,
+         use_spreadsheet_gender_gap=(use_spreadsheet_m-use_spreadsheet_f)/use_spreadsheet_m,
+         write_program_gender_gap=(write_program_m-write_program_f)/write_program_m) %>%
+  select(-ends_with('_m'),-ends_with('_f')) %>%
+  fix_adm0 
 
-setdiff(sdg4$country,all_countries)
+#setdiff(sdg4$country,all_countries)
+
+rm(rename_df)
