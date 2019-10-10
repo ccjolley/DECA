411216100ce6161ed2dfff9a2098bc73ef0b7293
Craig Jolley <jolleycraig@gmail.com>
2019-10-10 11:25:56 -0400

    Get nice-looking PCA plot

--- pca.R
+++ pca.R
@@ -1,10 +1,13 @@
+library(ggplot2)
+library(ggrepel)
+library(mice)
 # Combine all data series, impute missing data, perform PCA
 
 scripts <- c('utils.R','a4ai.R','eiu.R','fotn.R','gsma.R','imf.R','itu.R',
              'rsf.R','sdg4.R','vdem.R','wb.R','wef.R')
 for (s in scripts) { source(s) }
 
-all <- full_join(a4ai,eiu,by='country') %>%
+all <- full_join(a4ai,eiu,by='country') %>% # most country duplicates present after this step
   full_join(fotn,by='country') %>%
   full_join(gsma,by='country') %>%
   full_join(imf,by='country') %>%
@@ -15,14 +18,11 @@ all <- full_join(a4ai,eiu,by='country') %>%
   full_join(wb,by='country') %>%
   full_join(wef,by='country') 
 
-# setdiff(all$country,all_countries)
-# duplicate names: overall, infrastructure, ind_internet
-
 ################################################################################
 # Function that removes columns that are NA for more than a given fraction of 
 # USAID countries
 ################################################################################
-trim_columns <- function(df,frac,keep_list=NULL {
+trim_columns <- function(df,frac,keep_list=NULL) {
   # if no keep_list provided, include all countries
   if (is.null(keep_list)) {
     keep_list <- df$country
@@ -35,17 +35,82 @@ trim_columns <- function(df,frac,keep_list=NULL {
   df %>% select(keep_names)
 }
 
-# TODO: function that removes non-USAID countries for which more than a given fraction
-# of rows are missing
+# How many columns to keep?
+x <- 0.05*(1:20)
+y <- sapply(x, function(f) {
+  trim_columns(all,f,usaid_countries) %>% ncol
+})
+qplot(x,y) # try 0.75 for now
 
-# TODO: use these two functions to pare down data frame
+all_trim1 <- trim_columns(all,0.75,usaid_countries)
+c(ncol(all),ncol(all_trim1))
+# takes us from 244 columns to 80
 
-# TODO: use mice to impute (m=1 for now)
+###############################################################################
+# Function that removes non-USAID countries for which more than a given 
+# fraction of rows are missing. I'm willing to work a little harder to impute
+# missing values for USAID countries, but I'm not going to spend that effort
+# on Montserrat, for example.
+###############################################################################
+trim_rows <- function(df,frac,keep_list=c()) {
+  tmp <- df %>% filter(!country %in% keep_list)
+  missing <- tmp %>% 
+    select(-country) %>%
+    is.na %>%
+    rowMeans
+  also_keep <- tmp$country[missing <= (1-frac)]
+  df %>% filter(country %in% c(keep_list,also_keep))
+}
 
-# TODO: run prcomp
+all_trim2 <- trim_rows(all_trim1,0.75,usaid_countries)
+c(nrow(all_trim1),nrow(all_trim2),length(usaid_countries))
+# took us from 255 countries to 156, of which 100 are USAID countries
 
-# TODO: visualize first two dimensions; mark out Kenya as well as extreme cases
+###############################################################################
+# Imputation using MICE
+###############################################################################
+all_imp <- all_trim2 %>%
+  select(-country) %>%
+  mice(m=1,seed=1234)
 
-# TODO: regressions to help interpret first PCs
+imputed <- complete(all_imp,1) 
 
-# TODO: save PC1-2 to a text file
+###############################################################################
+# Principal components
+###############################################################################
+pr <- prcomp(imputed,center=TRUE,scale=TRUE)
+plot(pr) 
+summary(pr)
+# As we'd hope, lots of variation in the first PC; about 50% in the first two
+
+f <- 0.03
+plotme <- tibble(country=all_trim2$country,
+                 pc1=pr$x[,1],
+                 pc2=pr$x[,2]) %>%
+  mutate(color=ifelse(country=='Kenya','b','c'),
+         color=ifelse(pc1 < quantile(pc1,probs=f) | pc1 > quantile(pc1,probs=1-f) |
+                      pc2 < quantile(pc2,probs=f) | pc2 > quantile(pc2,probs=1-f),'a',color),
+         highlight= (color != 'c'),
+         label=ifelse(highlight,country,NA),
+         pc1=-pc1 # Need to do this was obvious looking at the plot
+  )
+
+ggplot(plotme,aes(x=pc1,y=pc2,color=color,label=label)) +
+  geom_point(size=1) +
+  theme_USAID + colors_USAID +
+  theme(legend.position = 'none') +
+  geom_text_repel() +
+  xlab('PC1: Digital sophistication') +
+  ylab('PC2: Digital openness')
+
+pc1_cor <- sapply(setdiff(names(all_trim2),'country'),
+       function(x) cor(all_trim2[,x],plotme$pc1,use='complete.obs')) %>%
+  sort()
+# Strongest correlations with Networked Readiness Index, Mobile Connectivity Index,
+# anti-correlations with various findex access gaps
+
+pc2_cor <- sapply(setdiff(names(all_trim2),'country'),
+                  function(x) cor(all_trim2[,x],plotme$pc2,use='complete.obs')) %>%
+  sort()
+# Strongest correlations with press freedom and government filtering capacity
+
