3cc805a27aafabb9c35d78c6b74d0edc3f60ffa0
Craig Jolley <jolleycraig@gmail.com>
2019-10-09 21:52:47 -0400

    Added civil liberties series to vdem.R (and renamed it)

--- vdem.R
+++ vdem.R
@@ -24,10 +24,23 @@ library(tidyverse)
 vdem_digital <- c('v2smgovfilcap','v2smgovfilprc','v2smgovshutcap','v2smgovshut','v2smgovsm','v2smgovsmalt','v2smgovsmmon',
                   'v2smgovsmcenprc','v2smgovcapsec','v2smpolcap','v2smregcon','v2smprivex','v2smprivcon','v2smregcap',
                   'v2smregapp','v2smlawpr','v2smdefabu')
-vdem <- extract_vdem(name_pattern='^v2sm.*') %>%
+vdem1 <- extract_vdem(name_pattern='^v2sm.*') %>%
   select(vdem_country_name,year,vdem_digital) %>%
   rename(country=vdem_country_name) %>%
   na.omit %>%
   group_by(country) %>%
   arrange(year) %>%
-  summarize_all(last)
+  summarize_all(last) %>%
+  fix_adm0
+
+vdem2 <- extract_vdem(name_pattern='v2x_c.*') %>%
+  select(vdem_country_name,year,v2x_civlib,v2x_clpol,v2x_clpriv) %>%
+  rename(country=vdem_country_name) %>%
+  na.omit %>%
+  group_by(country) %>%
+  arrange(year) %>%
+  summarize_all(last) %>% 
+  fix_adm0
+
+vdem <- full_join(vdem1,vdem2,by='country')
+rm(vdem1,vdem2,vdem_digital)
