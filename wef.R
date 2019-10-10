cbb57f641fa9cdedacf88579f4f64c290719930d
Craig Jolley <jolleycraig@gmail.com>
2019-10-09 21:40:51 -0400

    Added some additional series to wef.R

--- wef.R
+++ wef.R
@@ -14,16 +14,19 @@ wef_laws <- read_csv('data/laws_relating_to_icts.csv') %>%
 wef_nri <- read_excel('data/WEF_NRI_2012-2016_Historical_Dataset.xlsx',sheet=2,skip=3) %>%
   rename(code=`Code NRI 2016`) %>%
   filter(Attribute=='Value',
-         code %in% c('A','B','C','D','NRI'),
+         code %in% c('A','B','C','D','NRI','3.03','6.01','6.02','6.05','6.06'),
          Edition==2016) %>%
   select(code,Albania:Zimbabwe) %>%
   melt(id.var='code') %>%
   dcast(variable ~ code) %>% 
-  rename(country=variable,nri_enviro=A,nri_readiness=B,nri_usage=C,nri_impact=D,nri=NRI) %>%
+  rename(country=variable,nri_enviro=A,nri_readiness=B,nri_usage=C,nri_impact=D,nri=NRI,
+         bandwidth=`3.03`,mobile_subs=`6.01`,internet_users=`6.02`,fixed_internet=`6.05`,
+         mobile_broadband=`6.06`) %>%
   na.omit %>%
+  mutate_at(2:11,as.numeric) %>%
   fix_adm0
-
-# TODO: there are some other NRI components recommended in "Access and use"
 
 wef <- full_join(wef_laws,wef_nri,by='country')
 rm(wef_laws,wef_nri)
+
+# setdiff(wef$country,all_countries)
