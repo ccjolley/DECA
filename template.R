98aa2670a75be50399587f7d5909e3b7e88442e1
Craig Jolley <jolleycraig@gmail.com>
2019-11-01 17:53:21 -0400

    Add template.R

---
+++ template.R
@@ -0,0 +1,45 @@
+source('plots.R')
+
+###############################################################################
+# This is a place to collect all of the plots I need for the desk review 
+# template.
+###############################################################################
+
+cname <- 'Kenya'
+highlight_list <- c('Kenya','Uganda','United Republic of Tanzania','Ethiopia',
+                    'Somalia','Rwanda','Burundi')
+
+# access scatterplot
+
+# access indicator plot
+
+# adoption time series plot
+
+# affordability indicators
+
+# digital literacy indicators
+
+# WEF private sector
+
+# WEF public sector
+
+# freedom scatterplot
+
+# freedom indicators
+
+# cybersecurity indicators
+
+# findex indicators
+
+# findex barriers
+
+# EIU Global Microscope
+
+# GSMA MMRI
+
+# Postal scatterplot
+
+# Governance indicators
+
+# Gender scatterplot
+
