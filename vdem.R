c6ec5995738c8da7fa3f7e379a754632531e4894
Craig Jolley <jolleycraig@gmail.com>
2019-11-08 16:32:04 -0500

    Add some more indices from VDem

--- vdem.R
+++ vdem.R
@@ -19,11 +19,29 @@ library(tidyverse)
 # v2smregapp (gov online content regulation approach)
 # v2smlawpr (defamation protection)
 # v2smdefabu (abuse of defamation/copyright law by elites)
+# v2smgovdom     Government dissemination of false information domestic         
+# v2smgovab      Government dissemination of false information abroad           
+# v2smpardom     Party dissemination of false information domestic              
+# v2smparab      Party dissemination of false information abroad                
+# v2smfordom     Foreign governments dissemination of false information         
+# v2smforads     Foreign governments ads                                        
+# v2smonex       Online media existence                                         
+# v2smonper      Online media perspectives                                      
+# v2smmefra      Online media fractionalization                                 
+# v2smorgviol    Use of social media to organize offline violence               
+# v2smorgavgact  Average people’s use of social media to organize offline action
+# v2smorgelitact Elites’ use of social media to organize offline action         
+# v2smcamp       Party/candidate use of social media in campaigns               
+# v2smarrest     Arrests for political content                                  
+# v2smpolsoc     Polarization of society                                        
+# v2smpolhate    Political parties hate speech 
 
 
 vdem_digital <- c('v2smgovfilcap','v2smgovfilprc','v2smgovshutcap','v2smgovshut','v2smgovsm','v2smgovsmalt','v2smgovsmmon',
                   'v2smgovsmcenprc','v2smgovcapsec','v2smpolcap','v2smregcon','v2smprivex','v2smprivcon','v2smregcap',
-                  'v2smregapp','v2smlawpr','v2smdefabu')
+                  'v2smregapp','v2smlawpr','v2smdefabu','v2smgovdom','v2smgovab','v2smpardom','v2smparab','v2smfordom',
+                  'v2smforads','v2smonex','v2smonper','v2smmefra','v2smorgviol','v2smorgavgact','v2smorgelitact','v2smcamp',
+                  'v2smarrest','v2smpolsoc','v2smpolhate')
 vdem1 <- extract_vdem(name_pattern='^v2sm.*') %>%
   select(vdem_country_name,year,vdem_digital) %>%
   rename(country=vdem_country_name) %>%
